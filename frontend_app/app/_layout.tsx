4a75038a14e6d002163babdf17df88776ceb9635
ihyeeun <oioigl@naver.com>
2026-02-25 18:38:46 +0900

    feat: 앱 부팅시 토큰여부로 로그인 상태 렌더링

--- frontend_app/app/_layout.tsx
+++ frontend_app/app/_layout.tsx
@@ -1,6 +1,34 @@
-import { Stack } from "expo-router";
+import { loadAccessToken } from "@/features/auth/store/tokenStore";
+import { router, Stack } from "expo-router";
+import { useEffect, useState } from "react";
 
 export default function RootLayout() {
+  const [hasAuthToken, setHasAuthToken] = useState(false);
+
+  useEffect(() => {
+    let cancelled = false;
+
+    (async () => {
+      try {
+        const token = await loadAccessToken();
+
+        if (cancelled) return;
+
+        if (token) {
+          router.replace("/(tabs)/home");
+        } else {
+          router.replace("/(auth)/login");
+        }
+      } finally {
+        if (!cancelled) setHasAuthToken(true);
+      }
+    })();
+
+    return () => {
+      cancelled = true;
+    };
+  }, []);
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
