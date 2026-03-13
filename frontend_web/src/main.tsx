eae94c763f4145a5de5ae0ec93d15fd9e9ee4ff7
ihyeeun <oioigl@naver.com>
2026-03-13 15:41:28 +0900

    feat: 웹/tanstack query 설정

--- frontend_web/src/main.tsx
+++ frontend_web/src/main.tsx
@@ -1,11 +1,15 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./shared/styles/tokens.css";
 import "./index.css";
 import App from "./App.tsx";
+import { queryClient } from "@/shared/api/queryClient";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 );
