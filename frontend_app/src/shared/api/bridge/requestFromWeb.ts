36639f81e64e68174ac044a8678b7adb285d8552
ihyeeun <oioigl@naver.com>
2026-03-16 18:23:21 +0900

    chore: 중복되는 try/catch 제거

--- frontend_app/src/shared/api/bridge/requestFromWeb.ts
+++ frontend_app/src/shared/api/bridge/requestFromWeb.ts
@@ -1,21 +1,15 @@
 import { apiClient } from "@/src/shared/api/apiClient";
-import { isAxiosError } from "axios";
 import type { BridgeRequestPayload } from "./bridge.types";
 
 export async function requestFromWeb(payload: BridgeRequestPayload) {
   const { endpoint, method, body, params } = payload;
 
-  try {
-    const response = await apiClient.request({
-      url: endpoint,
-      method,
-      params,
-      data: body,
-    });
+  const response = await apiClient.request({
+    url: endpoint,
+    method,
+    params,
+    data: body,
+  });
 
-    return response.data;
-  } catch (error) {
-    throw error;
-    //TODO 여기에 서버 에러 메세지를 반환하도록 하고 싶긴 한데..!
-  }
+  return response.data;
 }
