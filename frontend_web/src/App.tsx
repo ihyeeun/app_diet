eecd0b6d7a3a433ddf1608fa78e1dd0ddf2ea4f8
ihyeeun <oioigl@naver.com>
2026-03-22 00:35:33 +0900

    feat: 전역 input에 정의된 문자만 입력되도록 설정

--- frontend_web/src/App.tsx
+++ frontend_web/src/App.tsx
@@ -2,11 +2,17 @@ import { useEffect } from "react";
 import "./App.css";
 import AppRouter from "./router/AppRouter";
 import { initNativeBridgeListener } from "@/shared/api/bridge/nativeBridge";
+import { initInputCharacterRestriction } from "@/shared/utils/inputCharacterRestriction";
 
 export default function App() {
   useEffect(() => {
-    const cleanup = initNativeBridgeListener();
-    return cleanup;
+    const cleanupNativeBridgeListener = initNativeBridgeListener();
+    const cleanupInputCharacterRestriction = initInputCharacterRestriction();
+
+    return () => {
+      cleanupInputCharacterRestriction();
+      cleanupNativeBridgeListener();
+    };
   }, []);
 
   return (
