f63464689952e4bd880fd8151b94c0a676bd4585
ihyeeun <oioigl@naver.com>
2026-04-18 13:01:22 +0900

    feat: 스플래시, 로그인 화면 로고 추가

--- frontend_app/app/(auth)/login.tsx
+++ frontend_app/app/(auth)/login.tsx
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View } from "react-native";
 import { KakaoLoginButton } from "@/features/auth/components/KakaoLoginButton";
 import { AppleLoginButton } from "@/features/auth/components/AppleLoginButton";
 import { router } from "expo-router";
+import LoginLogo from "@/assets/images/login-logo.svg";
+import LoginImage from "@/assets/images/login-image.svg";
 
 export default function LoginPage() {
   const startAppleLogin = React.useCallback(() => {
@@ -16,24 +18,30 @@ export default function LoginPage() {
   return (
     <View style={styles.container}>
       <View style={styles.content}>
-        <View style={styles.socialButtonGroup}>
-          <KakaoLoginButton onPress={() => router.push("/kakaoLogin")} />
-          <AppleLoginButton onPress={startAppleLogin} />
+        <View style={styles.hero}>
+          <LoginLogo width={"100%"} />
+          <LoginImage width={"100%"} />
         </View>
+        <View style={styles.bottom}>
+          <View style={styles.socialButtonGroup}>
+            <KakaoLoginButton onPress={() => router.push("/kakaoLogin")} />
+            <AppleLoginButton onPress={startAppleLogin} />
+          </View>
 
-        <Text style={styles.agreementText}>
-          가입하면 Melo의{"\n"}
-          <Text
-            style={styles.linkText}
-            onPress={openTermsPage}
-            accessible={true}
-            accessibilityRole="link"
-            accessibilityLabel="이용약관 및 개인정보 처리방침"
-          >
-            이용약관 및 개인정보 처리방침
+          <Text style={styles.agreementText}>
+            가입하면 Melo의{"\n"}
+            <Text
+              style={styles.linkText}
+              onPress={openTermsPage}
+              accessible={true}
+              accessibilityRole="link"
+              accessibilityLabel="이용약관 및 개인정보 처리방침"
+            >
+              이용약관 및 개인정보 처리방침
+            </Text>
+            에 동의하게 됩니다.
           </Text>
-          에 동의하게 됩니다.
-        </Text>
+        </View>
       </View>
     </View>
   );
@@ -42,17 +50,26 @@ export default function LoginPage() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "center",
-    paddingHorizontal: 20,
     paddingBottom: 56,
     backgroundColor: "#ffffff",
   },
   content: {
+    flex: 1,
     width: "100%",
     alignItems: "center",
+    justifyContent: "space-between",
     gap: 20,
   },
+  hero: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    gap: 50,
+    width: "100%",
+    paddingTop: 120,
+    paddingBottom: 40,
+  },
+  bottom: { alignItems: "center", gap: 20, width: "100%" },
   socialButtonGroup: {
     gap: 12,
     alignItems: "center",
